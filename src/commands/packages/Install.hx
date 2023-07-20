765c8ba48cbd8d448e0c22b57fd7e553b240e23e
sidgames5 <saytleader@outlook.com>
2023-07-20 12:47:00 -0400

    Install command

--- src/commands/packages/Install.hx
+++ src/commands/packages/Install.hx
@@ -1,5 +1,10 @@
 package commands.packages;
 
+import structs.Package;
+import sys.io.Process;
+import sys.thread.Thread;
+import sys.FileSystem;
+import haxe.io.Bytes;
 import sys.io.File;
 import haxe.Json;
 import haxe.io.Path;
@@ -23,12 +28,46 @@ class Install implements Command {
 			versionsreq.onData = function(data:String) {
 				Sys.println("Fetched versions");
 				var versions = Json.parse(data);
-				var version = versions[0];
+				var version = versions[versions.length - 1];
 				Sys.println("Installing " + pkgname + " " + version);
+				var zipname = pkgname + "-" + version;
 				var zipreq = new Http(Path.join([pkgrepo.packagesURL, pkgname, version + ".zip"]));
-				zipreq.onData = function(data:String) {
-					Sys.println("Fetched zip");
-					trace(data);
+				zipreq.onData = function(data) {
+					Sys.println("Fetching zip");
+					FileSystem.createDirectory("/opt/escam/temp/");
+					// File.saveContent("/opt/escam/temp/" + pkgname + ".zip", data);
+					Sys.command("curl -o /opt/escam/temp/" + zipname + ".zip " + Path.join([pkgrepo.packagesURL, pkgname, version + ".zip"]));
+					Sys.command("cd /opt/escam/temp/ && unzip /opt/escam/temp/" + zipname + ".zip -d /opt/escam/temp/" + zipname);
+
+					var packagejson:Package = Json.parse(File.getContent(Path.join(["/opt/escam/temp/", zipname, "package.json"])));
+
+					var preparescript = packagejson.scripts.prepare;
+					var buildscript = packagejson.scripts.build;
+					var installscript = packagejson.scripts.install;
+					var postinstallscript = packagejson.scripts.postinstall;
+
+					var outfile = packagejson.outfile;
+
+					if (preparescript != null) {
+						Sys.println("Preparing build");
+						Sys.command(preparescript);
+					}
+					if (buildscript != null) {
+						Sys.println("Building package");
+						Sys.command("cd /opt/escam/temp/" + zipname + " && " + buildscript);
+					}
+					Sys.println("Installing package");
+					if (installscript != null) {
+						Sys.command("cd /opt/escam/temp/" + zipname + " && " + installscript);
+					} else {
+						// handle stuff
+						Sys.command("cd /opt/escam/temp/" + zipname + " && " + "cp " + outfile + " /usr/bin/" + pkgname);
+					}
+					if (postinstallscript != null) {
+						Sys.println("Running post-install script");
+						Sys.command(postinstallscript);
+					}
+					Sys.println("Installed " + pkgname + " " + version);
 				}
 				zipreq.onError = function(msg:String) {
 					Sys.println("Error fetching zip: " + msg);
