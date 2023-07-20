0857077bea1f4c5d0ec00fbfb3b97356501f47a1
sidgames5 <saytleader@outlook.com>
2023-07-20 14:36:15 -0400

    Add help command

--- src/commands/Help.hx
+++ src/commands/Help.hx
@@ -1,7 +1,13 @@
 package commands;
 
 class Help implements Command {
-	public function bind(args:Array<String>) {}
+	public function bind(args:Array<String>) {
+		Commands.execute(new Version());
+		Sys.println("Command summary");
+		Sys.println("install [i]            | installs a package");
+		Sys.println("add-repository [ar]    | adds a repository");
+		Sys.println("remove-repository [rr] | removes a repository");
+	}
 
 	public function new() {}
 }
