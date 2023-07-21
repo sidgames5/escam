4079b9eb26c2412d72e4a8a2b279ac797f08804d
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-07-20 22:59:11 -0400

    battery 🔋

--- src/commands/Version.hx
+++ src/commands/Version.hx
@@ -2,7 +2,7 @@ package commands;
 
 class Version implements Command {
 	public function bind(args:Array<String>) {
-		Sys.println("Escam " + Main.version);
+		Sys.println("🔋Escam " + Main.version);
 	}
 
 	public function new() {}
