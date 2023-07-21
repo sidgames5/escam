06a708f77fea34dcbc61de0e65c144d67b951dbb
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-07-20 23:07:09 -0400

    slightly more helpful also i dont want to install “s package”

--- src/commands/Help.hx
+++ src/commands/Help.hx
@@ -4,12 +4,12 @@ class Help implements Command {
 	public function bind(args:Array<String>) {
 		Commands.execute(new Version());
 		Sys.println("Command summary");
-		Sys.println("install [alias i]            | installs s package");
+		Sys.println("install [alias i]            | installs a package");
 		Sys.println("remove [alias r]             | removes a package");
 		Sys.println("update [alias u]             | updates a package");
 		Sys.println("add-repository [alias ar]    | adds a repository");
 		Sys.println("remove-repository [alias rr] | removes a repository");
-		Sys.println("sync-repository [alias sr]   | syncs a repository");
+		Sys.println("sync-repository [alias sr]   | fetches the latest version of a repository");
 	}
 
 	public function new() {}
