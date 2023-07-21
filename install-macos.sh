a8ab27241d23808ec895261f05a6049636b2fee7
sidgames5 <saytleader@outlook.com>
2023-07-21 16:21:22 -0400

    Update macos installer script

--- install-macos.sh
+++ install-macos.sh
@@ -1,5 +1,5 @@
-sudo brew install haxe
-sudo brew install wget
+brew install haxe
+brew install wget
 wget https://github.com/sidgames5/escam/archive/refs/tags/0.4.0.zip
 unzip 0.4.0.zip
 cd escam-0.4.0/
