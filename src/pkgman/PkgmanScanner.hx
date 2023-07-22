5c52f07fe9f1b0cf8651bca65788976203b78afd
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-07-22 14:07:13 -0400

    allow choice of package manager: #1

--- src/pkgman/PkgmanScanner.hx
+++ src/pkgman/PkgmanScanner.hx
@@ -5,14 +5,21 @@ import sys.FileSystem;
 
 class PkgmanScanner {
 	public static function getLocalPackageManager():String {
+		var packagemans:Array<String> = [];
 		for (folder in ScanFolders.get()) {
 			for (pkgman in PackageManager.get()) {
 				Sys.println("Checking " + Path.join([folder, pkgman]));
 				if (FileSystem.exists(Path.join([folder, pkgman]))) {
-					return pkgman;
+					if (!packagemans.contains(pkgman)) {
+						packagemans.push(pkgman)
+					}
 				}
 			}
 		}
-		return null;
+		if (packagemans.length < 1) {
+			return null;
+		} else {
+			return packagemans;
+		}
 	}
 }
