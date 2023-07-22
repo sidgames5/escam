eb595b2ddb0bd814b1f58627ccb09638b52a3b96
sidgames5 <saytleader@outlook.com>
2023-07-21 21:18:12 -0400

    Update install scripts

--- install-linux-arch.sh
+++ install-linux-arch.sh
@@ -1,8 +1,8 @@
 sudo pacman -S haxe
 sudo pacman -S wget
-wget https://github.com/sidgames5/escam/archive/refs/tags/0.4.0.zip
-unzip 0.4.0.zip
-cd escam-0.4.0/
+wget https://github.com/sidgames5/escam/archive/refs/tags/0.4.1.zip
+unzip 0.4.1.zip
+cd escam-0.4.1/
 haxelib install hxcpp
 haxe build.hxml
 cd bin/
